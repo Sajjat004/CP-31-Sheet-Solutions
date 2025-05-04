e56006b55e1fca928a9e24815ca2266dd6455cac
sajjat-hossen <sajjat.hossen@brainstation-23.com>
2025-05-04 21:14:43 +0600

    05 May, 2025

--- 1600 Rating Problems/Decreasing String.cpp
+++ 1600 Rating Problems/Decreasing String.cpp
@@ -1,10 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string getStringFromStack(stack<char>& st) {
+  string s = "";
+  while (!st.empty()) {
+    s += st.top();
+    st.pop();
+  }
+
+  reverse(s.begin(), s.end());
+
+  return s;
+}
+
+char solve() {
+  string s; cin >> s;
+  long long k; cin >> k;
+  int n = s.size();
+
+  int operations = 0;
+  long long m = n;
+  while (k > m) {
+    k -= m;
+    m--;
+    operations++;
+  }
+
+  stack<char> st;
+  for (char ch : s) {
+    while (!st.empty() and st.top() > ch and operations > 0) {
+      st.pop();
+      operations--;
+    }
+    st.push(ch);
+  }
+
+  string t = getStringFromStack(st);
+  return t[k - 1];
+}
+
 int32_t main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
+
+  int tests; cin >> tests;
+  while (tests--) {
+    cout << solve();
+  }
+  cout << '\n';
     
   return 0;
 }
 
-// Problem Link: 
+// Problem Link: https://codeforces.com/problemset/problem/1886/C
--- 1600 Rating Problems/Good Triples.cpp
+++ 1600 Rating Problems/Good Triples.cpp
@@ -1,10 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
+void preCal(vector<int>& cnt) {
+  for (int d = 0; d < 10; ++d) {
+    for (int i = 0; i < 10; ++i) {
+      for (int j = 0; j < 10; ++j) {
+        for (int k = 0; k < 10; ++k) {
+          if (i + j + k == d) cnt[d]++;
+        }
+      }
+    }
+  }
+}
+
+ll solve(vector<int>& cnt) {
+  int n; cin >> n;
+  ll ans = 1;
+
+  while (n) {
+    int digit = n % 10;
+    ans *= cnt[digit];
+    n /= 10;
+  }
+
+  return ans;
+}
+
 int32_t main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-    
+
+  vector<int> cnt(10, 0);
+  preCal(cnt);
+
+  int tests; cin >> tests;
+  while (tests--) {
+    cout << solve(cnt) << '\n';
+  }
+
   return 0;
 }
 
-// Problem Link: 
+// Problem Link: https://codeforces.com/problemset/problem/1907/E
